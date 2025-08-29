750b67cb64715431ff26556c7be92e0a75dc957c
Rezuanul Islam Fahim <rifahim98@gmail.com>
2025-08-29 18:21:40 +0600

    feat: add NotFound page and Projects page with project details and filtering functionality

--- vite.config.ts
+++ vite.config.ts
@@ -1,40 +1,22 @@
-import { defineConfig, Plugin } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { createServer } from "./server";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
-    fs: {
-      allow: ["./client", "./shared"],
-      deny: [".env", ".env.*", "*.{crt,pem}", "**/.git/**", "server/**"],
-    },
   },
+  plugins: [react()],
+  base: "/personal-portfolio/",
   build: {
-    outDir: "dist/spa",
+    outDir: "dist",
+    emptyOutDir: true,
   },
-  plugins: [react(), expressPlugin()],
-  base: "/",
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./client"),
-      "@shared": path.resolve(__dirname, "./shared"),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 }));
-
-function expressPlugin(): Plugin {
-  return {
-    name: "express-plugin",
-    apply: "serve", // Only apply during development (serve mode)
-    configureServer(server) {
-      const app = createServer();
-
-      // Add Express app as middleware to Vite dev server
-      server.middlewares.use(app);
-    },
-  };
-}
