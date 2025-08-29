c627b7810da347373b4ba31dd8b64c6157c05efb
Rezuanul Islam Fahim <rifahim98@gmail.com>
2025-08-29 17:37:57 +0600

    Set base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,7 @@ export default defineConfig(({ mode }) => ({
     outDir: "dist/spa",
   },
   plugins: [react(), expressPlugin()],
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./client"),
